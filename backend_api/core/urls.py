708d8e58f21c60d03031465ebf67146bea32ce09
Tatyana29 <75305329+Tatyana29@users.noreply.github.com>
2024-06-06 08:34:50 +0300

    Delete backend_api directory

--- backend_api/core/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.home, name='home'),
-    path('todos/', views.todos_list, name='todos_list'),
-    path('todos/<str:pk>/', views.todo, name='todo'),
-    
-]
